ebae694e3c94fb654a4ab4c06324f8b90ec3c326
Ana Melicia <meliciaana@gmail.com>
2024-10-09 17:07:59 -0300

    Desafio concluído 2

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        Smartphone iphone = new Iphone("123456789", "iPhone 14");
+        iphone.Ligar();
+        iphone.InstalarAplicativo("WhatsApp");
+        
+        Console.WriteLine(); // Linha em branco para melhor visualização
+        
+        Smartphone nokia = new Nokia("987654321", "Nokia 3310");
+        nokia.Ligar();
+        nokia.InstalarAplicativo("Snake");
+    }
+}
